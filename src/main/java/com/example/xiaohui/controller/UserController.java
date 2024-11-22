8d51459f5c043fd5fed9161e0a30cd5575e3115f
phpoh <hurrywang@HurrydeMacBook-Pro.local>
2024-11-23 04:24:17 +0800

    V1.3

--- src/main/java/com/example/xiaohui/controller/UserController.java
+++ src/main/java/com/example/xiaohui/controller/UserController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Tag(name = "用户管理", description = "用户管理API")
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("/api")
@@ -21,12 +20,7 @@ public class UserController {
     private final String USERNAME = "admin";
     private final String PASSWORD = "123123";
 
-    @Operation(summary = "用户登录",
-            description = "用户通过提供用户名和密码进行登录，成功则返回用户信息和 token。",
-            responses = {
-                    @ApiResponse(responseCode = "200", description = "登录成功，返回用户信息和 token"),
-                    @ApiResponse(responseCode = "401", description = "用户名或密码错误")
-            })
+
     @GetMapping("/login")
     public User login(@Parameter(description = "登录用户的用户名和密码") User user) {
         if (USERNAME.equals(user.getUsername()) && PASSWORD.equals(user.getPassword())) {
@@ -37,15 +31,8 @@ public class UserController {
         return null;
     }
 
-    @Operation(summary = "用户与AI对话",
-            description = "用户发送消息后，AI系统处理并返回回复消息。",
-            responses = {
-                    @ApiResponse(responseCode = "200", description = "AI成功回复消息"),
-                    @ApiResponse(responseCode = "500", description = "服务器错误")
-            })
     @PostMapping("/chat")
-    public Response handleChatRequest(
-            @RequestBody @Parameter(description = "用户发送的消息") MessageRequest request) {
+    public Response handleChatRequest(@RequestBody  MessageRequest request) {
         // 获取前端传递的消息内容
         String message = request.getMessage();
 
