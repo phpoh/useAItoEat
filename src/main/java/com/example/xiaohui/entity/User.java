ded864120fd61ff6daa57dfa064a95d413989888
phpoh <1352808998@qq.com>
2025-01-02 15:37:49 +0800

    数据量小 暂时放弃使用mybatis

--- src/main/java/com/example/xiaohui/entity/User.java
+++ src/main/java/com/example/xiaohui/entity/User.java
@@ -1,20 +1,17 @@
 package com.example.xiaohui.entity;
 
-import com.baomidou.mybatisplus.annotation.TableId;
-import com.baomidou.mybatisplus.annotation.TableName;
+
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
-@TableName(value = "user")
 public class User {
 
-    @TableId
     private Long id;
 
     private String username;
 
     private String password;
 
-
+    private String status;
 }
